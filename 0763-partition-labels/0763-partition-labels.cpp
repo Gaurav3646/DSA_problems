28c49f83db17d5bb946ea131a10a6125e0786d81
Gaurav3646 <91468455+Gaurav3646@users.noreply.github.com>
2022-12-08 09:56:40 +0530

    Time: 10 ms (39.41%), Space: 6.7 MB (68.02%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // static bool cmp(pair<int,int> p1, pair<int, int> p2){
+    //       return p1.second<p2.second;
+    // }
+    vector<int> partitionLabels(string s) {
+        vector<int> p(26, INT_MIN);
+        for(int i=0; i<s.length(); i++){
+             p[s[i]-'a'] = max(i,  p[s[i]-'a']);
+        }
+//         sort(p.begin(), p.end(), cmp);
+//         int i= 
+        vector<int> ans;
+//         int last =
+//         for(int i=0; i<26;  i++){
+//             if(p[i].first==INT_MAX)continue;
+            
+//         }
+        int l=-1;
+        int r=-1;
+         
+        for(int i=0; i<s.length(); i++){
+              if(r<i){
+                if(l!=-1 && r!=-1)
+                 ans.push_back(r-l+1);
+                  r = i;
+                  l = i;
+              }
+              r = max(r,p[s[i]-'a']);
+            
+             
+            
+            
+        }
+    if(l!=-1 && r!=-1)
+        ans.push_back(r-l+1);
+        return ans;
+    }
+};
