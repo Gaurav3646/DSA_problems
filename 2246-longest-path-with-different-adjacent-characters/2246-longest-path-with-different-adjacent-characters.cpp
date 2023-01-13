aeb3048d206fbc5293dd42fe9ee0514476e23e02
Gaurav3646 <91468455+Gaurav3646@users.noreply.github.com>
2023-01-13 13:56:42 +0530

    Time: 386 ms (94.38%), Space: 181.2 MB (65.77%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-        int maxi = 1;
-   pair<int, char> solve(int u, vector<vector<int>>& adj, string &s){
-          pair<int, char> ans = {0, '#'};
-       for(auto &v : adj[u]){
-      pair<int, char> x = solve(v, adj, s);
-           if(x.second!=s[u]){
-              maxi = max(ans.first+1+x.first,  maxi);
-               if(ans.first<x.first)
-                   ans = x;
-           }
+//         int maxi = 1;
+//    pair<int, char> solve(int u, vector<vector<int>>& adj, string &s){
+//           pair<int, char> ans = {0, '#'};
+//        for(auto &v : adj[u]){
+//       pair<int, char> x = solve(v, adj, s);
+//            if(x.second!=s[u]){
+//               maxi = max(ans.first+1+x.first,  maxi);
+//                if(ans.first<x.first)
+//                    ans = x;
+//            }
         
+//        }
+//        ans.first++;
+//        ans.second = s[u];
+//       return ans;
+//    } 
+            int maxi = 1;
+   int solve(int u, vector<vector<int>>& adj, string &s){
+          int ans = 0;
+       for(auto &v : adj[u]){
+     int x = solve(v, adj, s);
+           if(s[v]!=s[u]){
+              maxi = max(ans+1+x,  maxi);
+               if(ans<x)
+                 ans = x;
+           }
        }
-       ans.first++;
-       ans.second = s[u];
+       ans++;
+        maxi = max(maxi, 1);
       return ans;
    } 
+     
         
     int longestPath(vector<int>& parent, string s) {
         maxi = 1;
@@ -23,7 +39,7 @@ public:
         for(int i=1; i<parent.size(); i++){
             adj[parent[i]].push_back(i);
         }
-        pair<int,char> ans = solve(0, adj, s);
+        int ans = solve(0, adj, s);
         return maxi;
     }
 };
