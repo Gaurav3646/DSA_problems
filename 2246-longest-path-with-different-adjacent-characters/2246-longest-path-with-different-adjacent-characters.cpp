ef4114e0d599f87cb227dbb95fe4e961eea566f8
Gaurav3646 <91468455+Gaurav3646@users.noreply.github.com>
2023-01-13 13:37:27 +0530

    Time: 409 ms (88.51%), Space: 188.3 MB (52.57%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-        int maxi = 0;
-   pair<int, char> solve(int u, vector<vector<int>>& adj, int parent, string &s){
-        // if(!root) return 0;
+        int maxi = 1;
+   pair<int, char> solve(int u, vector<vector<int>>& adj, string &s){
           pair<int, char> ans = {0, '#'};
        for(auto &v : adj[u]){
-           if(v==parent) continue;
-      pair<int, char> x = solve(v, adj, u, s);
+      pair<int, char> x = solve(v, adj, s);
            if(x.second!=s[u]){
               maxi = max(ans.first+1+x.first,  maxi);
                if(ans.first<x.first)
                    ans = x;
            }
-        // if(root->val==left && root->val==right)
-        //       maxi = max(maxi, 1);
-        // else if(root->val==left.second){
-        //       maxi = max(maxi, 1+right.first);  
-        //     }
-        // else if(root->val==right.second){
-        //       maxi = max(maxi, 1+left.first);   
-        //     }
-        //  else{
-        //  maxi = max(maxi, temp); 
-        //  } 
+        
        }
-       maxi = max(maxi, 1);
-       // cout<<maxi<<endl;
        ans.first++;
        ans.second = s[u];
       return ans;
    } 
         
     int longestPath(vector<int>& parent, string s) {
-        maxi = INT_MIN;
+        maxi = 1;
         vector<vector<int>> adj(parent.size());
         for(int i=1; i<parent.size(); i++){
-            
             adj[parent[i]].push_back(i);
         }
-        pair<int,char> ans = solve(0, adj, -1, s);
+        pair<int,char> ans = solve(0, adj, s);
         return maxi;
     }
 };
