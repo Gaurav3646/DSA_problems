e83001e02daa7df7fed6d1e7e352cecdd2ef4a34
Gaurav3646 <91468455+Gaurav3646@users.noreply.github.com>
2023-01-21 22:55:09 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (42.06%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     vector<string> strs;
     void addPoints(string &s, int i, string p, int pt){
-          if(pt==0 && i<s.length()){
-              int y = 0;
-              if(i!=s.length()-1 && s[i]=='0') return;
-             while(i<s.length() && y<=255){
-                y = y*10 + s[i]-'0';
-                 p += s[i];
-                 i++;
-             }
-              if(y<=255){
+          if(pt==0 && i>=s.length()){
+             //  int y = 0;
+             //  if(i!=s.length()-1 && s[i]=='0') return;
+             // while(i<s.length() && y<=255){
+             //    y = y*10 + s[i]-'0';
+             //     p += s[i];
+             //     i++;
+             // }
+             //  if(y<=255){
+              p.pop_back();
                   strs.push_back(p);
-              }
+               p += '.';
+              // }
           }
-        if(i>=s.length()) return;
+        if(pt==0) return;
         int x = 0;
         if(s[i]!='0'){
-        for(int j=i; j<s.length()-1 && (x*10+s[j]-'0')<=255; j++){
+        for(int j=i; j<s.length() && (x*10+s[j]-'0')<=255; j++){
             x = x*10+s[j]-'0';
             p += s[j];
              addPoints(s, j+1, p+'.', pt-1);
@@ -29,7 +31,7 @@ public:
         }
       }
     vector<string> restoreIpAddresses(string s) {
-         addPoints(s, 0, "", 3);  
+         addPoints(s, 0, "", 4);  
         return strs;
     }
 };
