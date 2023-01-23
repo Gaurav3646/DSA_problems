3ee44442af8b932d785729419f36e060f883bda2
Gaurav3646 <91468455+Gaurav3646@users.noreply.github.com>
2023-01-23 20:52:17 +0530

    Time: 184 ms (83.22%), Space: 60.7 MB (86.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<unordered_set<int>> trusts(n+1);
-        for(int i=0; i<trust.size(); i++){
-            int u = trust[i][0];
-            int v = trust[i][1];
-          trusts[v].insert(u);
-        }
-      int judge = -1;
-        for(int i=1; i<=n; i++){
-            if(trusts[i].size()==n-1){
-                int jd = true;
-                for(int j=1; j<=n; j++){
-                    if(i!=j && trusts[j].count(i)){
-                        jd = false;
-                    }
-                }
-                 if(jd) judge = i;  
-            }
+//         vector<unordered_set<int>> trusts(n+1);
+//         for(int i=0; i<trust.size(); i++){
+//             int u = trust[i][0];
+//             int v = trust[i][1];
+//           trusts[v].insert(u);
+//         }
+//       int judge = -1;
+//         for(int i=1; i<=n; i++){
+//             if(trusts[i].size()==n-1){
+//                 int jd = true;
+//                 for(int j=1; j<=n; j++){
+//                     if(i!=j && trusts[j].count(i)){
+//                         jd = false;
+//                     }
+//                 }
+//                  if(jd) judge = i;  
+//             }
            
         
+//         }
+//         return  judge;
+        
+        vector<int> inDegree(n+1);
+        vector<int> outDegree(n+1);
+        for(int i=0; i<trust.size(); i++){
+            inDegree[trust[i][1]]++;
+            outDegree[trust[i][0]]++;
+            
         }
-        return  judge;
+        for(int i=1; i<=n; i++){
+            if(inDegree[i]==n-1 && outDegree[i]==0) return i;
+        }
+        return -1;
        
     }
 };
