19145e55bb1ee893f4134441b81b55c815155947
Gaurav3646 <91468455+Gaurav3646@users.noreply.github.com>
2023-03-13 22:44:50 +0530

    Time: 243 ms (53.46%), Space: 84.2 MB (71.92%) - LeetHub

--- 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int maxNonOverlapping(vector<int>& arr, int target) {
          unordered_map<int, int> mp;
-        vector<int> prefix(arr.size());
-        prefix[0] = arr[0];
-        for(int i=1; i<arr.size(); i++){
-            prefix[i] = arr[i] + prefix[i-1];
-        }
+        // vector<int> prefix(arr.size());
+        // prefix[0] = arr[0];
+        // for(int i=1; i<arr.size(); i++){
+        //     prefix[i] = arr[i] + prefix[i-1];
+        // }
         mp[0] = -1;
         int k=-1;
         int count = 0;
+        int prefix =0;
         for(int i=0; i<arr.size(); i++){
-            if(mp.find(prefix[i]-target)!=mp.end() && mp[prefix[i]-target]>=k){
+            prefix +=arr[i];
+            if(mp.find(prefix-target)!=mp.end() && mp[prefix-target]>=k){
                 count++;
-                mp[prefix[i]] = i;
                 k = i;
             }
-            else{
-                mp[prefix[i]]=i;
-            }
+            mp[prefix] = i;
         }
         return count;
     }
