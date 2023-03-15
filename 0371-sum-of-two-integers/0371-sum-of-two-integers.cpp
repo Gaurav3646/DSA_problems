b642fd44fd522fba430a0d8a5aadd47aef567db1
Gaurav3646 <91468455+Gaurav3646@users.noreply.github.com>
2023-03-15 21:24:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (65.55%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int n1 = a;
-        int n2 = b;
-        int ans = 0;
-        int c = 0;
-        int i = 0;
-        while(i<32){
-             int x1 = (n1>>i)&1;
-             int x2 = (n2>>i)&1;
-            if(x1&&x2){
-                if(c==1)
-                ans = ans|(1<<i);
-                else
-                    c=1;
-   
-            }
-            else{
-               if(x1==0 && x2==0){
-                   if(c==1){
-                       ans = ans|(1<<i);
-                       c=0;
-                   }
-               }
-                else{
-                    if(c==0){
-                        ans = ans|(1<<i);
-                    }
-                }
-            }
-            i++;
+        // 2 , 3
+       unsigned int c = 0;
+        while(b){
+            c = a&b; // 2&3 --> 10
+            a = a^b; // 01
+            b = c<<1;  // 100 
         }
-        return ans;
+        return a;
     }
 };
