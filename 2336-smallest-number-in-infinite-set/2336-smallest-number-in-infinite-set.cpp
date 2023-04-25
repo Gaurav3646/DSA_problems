2a8df56d26b8a5866b38cbfa73e18f66740f3b78
Gaurav Verma <91468455+Gaurav3646@users.noreply.github.com>
2023-04-25 21:19:05 +0530

    Time: 77 ms (86.09%), Space: 35.5 MB (77.45%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,23 +1,28 @@
 class SmallestInfiniteSet {
 private:
-    
-    vector<int> vect;
+
     set<int> p;
-    int i;
+    int i = 1;
 public:
-    SmallestInfiniteSet() {
-       for(int i=1; i<=1000; i++){
-           p.insert(i);
-       }
-    }
+    // SmallestInfiniteSet() {
+    //    // for(int i=1; i<=1000; i++){
+    //    //     p.insert(i);
+    //    // }
+    //     i=1;
+    // }
     
     int popSmallest() {
+        if(!p.empty()){
      int x = *(p.begin());
         p.erase(p.begin());
+    
         return x;
+        }
+        return i++;
     }
     
     void addBack(int num) {
+        if(num<i)
        p.insert(num);
     }
 };
