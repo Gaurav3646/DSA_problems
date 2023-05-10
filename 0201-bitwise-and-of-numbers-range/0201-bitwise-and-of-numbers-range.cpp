b1a6215bd4a0e62f17536ba1aa5e12bc251c321f
Gaurav Verma <91468455+Gaurav3646@users.noreply.github.com>
2023-05-10 22:44:08 +0530

    Time: 13 ms (58.65%), Space: 5.9 MB (93.48%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
         int ans = 0;
+        int p = pow(2, 30);
         for(int i=30; i>=0; i--){
-            int l = (left>>i)&1;
-            int r = (right>>i)&1;
+            int l = left&p;
+            int r = right&p;
             // cout<<l<<" "<<r<<endl;
            if(l&r)
-              ans += 1<<i;
-           else if(l==0 && r==1) return ans; 
+              ans += p;
+           else if(l==0 && r==p) return ans; 
+            
+            // cout<<p<<endl;
+            p /=2;
+            
             
         }
         return ans;
     }
+
 };
