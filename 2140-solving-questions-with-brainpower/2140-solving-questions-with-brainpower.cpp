290bad260738db4f12d814e8b11f01932677e496
Gaurav Verma <91468455+Gaurav3646@users.noreply.github.com>
2023-05-12 22:00:41 +0530

    Time: 417 ms (12.13%), Space: 120.6 MB (60.90%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     long long mostPoints(vector<vector<int>>& q) {
-    long long dp[200001] = {};
-    for (int i = q.size() - 1; i >= 0; --i)
-        dp[i] = max(q[i][0] + dp[q[i][1] + i + 1], dp[i + 1]);
-    return dp[0];
-}
+       long long  dp[1000000] ={0};
+        for(int i=q.size()-1; i>=0; i--){
+            dp[i] = max(q[i][0] + dp[i+q[i][1]+1], dp[i+1]);
+        }
+        return dp[0];
+    }
 };
