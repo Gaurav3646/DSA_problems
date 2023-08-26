4acf83c74d844fe1bf1c0d79058bc0cc4ab21f99
Gaurav Verma <91468455+Gaurav3646@users.noreply.github.com>
2023-08-26 13:05:10 +0530

    Time: 40 ms (60.84%), Space: 18.4 MB (75.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   
+   static int MOD;
     int solve(string &s, string &t, int i, int j, vector<vector<int>> &dp){
         if(i>=t.size()){
             
@@ -16,9 +16,31 @@ public:
         return dp[j][i] = ans;
         
     }
+    
+    int solveTab(string &s, string &t){
+        vector<vector<int>> dp(t.size()+1, vector<int>(s.size()+1, 0));
+        for(int i=0; i<=s.size(); i++){
+            dp[t.size()][i] = 1;
+        }
+    for(int i=t.size()-1; i>=0; i--){
+        for(int j=s.size()-1; j>=0; j--){
+                 int ans =0;
+             if(t[i]==s[j]){
+                  ans += dp[i+1][j+1];
+              }
+              ans %= MOD;
+                ans += dp[i][j+1];
+            ans %= MOD;
+               dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+        
+    }
     int numDistinct(string s, string t) {
-                vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, -1));
-        return  solve(s, t, 0, 0, dp);
+        //         vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, -1));
+        // return  solve(s, t, 0, 0, dp);
+        return solveTab(s, t);
        
 
 //       for(int i=0; i<s.size(); i++){
@@ -31,3 +53,5 @@ public:
 //       }
     }
 };
+
+int Solution::MOD = 1e9+7;
