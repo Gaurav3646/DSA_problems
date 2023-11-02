4ccad779a0a820a139e5d3fd96fc269628f18ed4
Gaurav Verma <91468455+Gaurav3646@users.noreply.github.com>
2023-11-02 12:56:00 +0530

    Time: 38 ms (47.88%), Space: 15.6 MB (43.58%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-int solve(string &text1, string &text2){
-        vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length()+1, 0));
-        
-    for(int i=text1.length()-1; i>=0; i--)                 // base case filling up
-        dp[i][text2.length()] = dp[i+1][text2.length()] + text1[i];
-
-    for(int j=text2.length()-1; j>=0; j--)                 // base case filling up
-        dp[text1.length()][j] = dp[text1.length()][j+1] + text2[j];
-    
-        for(int i1=text1.length()-1; i1>=0; i1--){
-       for(int i2=text2.length()-1; i2>=0; i2--){
-           int mx = INT_MAX;
-        if(text1[i1]==text2[i2]){
-          mx = min(mx, dp[i1+1][i2+1]);
-        }
-        else{
-            
-           mx = min(text1[i1] + dp[i1+1][i2], text2[i2]+dp[i1][i2+1]);
-        }
-          
-         dp[i1][i2] = mx;  
-            // cout<<dp[i1][i2]<<endl;
-       }
-        }
-    
-//     for(int i=0; i<dp.size(); i++){
-//         for(int j=0; j<dp[0].size(); j++){
-//             cout<<dp[i][j]<<" ";
-//         }
-//         cout<<endl;
-//     }
-        return dp[0][0];
-    }
     int minimumDeleteSum(string s1, string s2) {
-     return solve(s1, s2);  
+        vector<vector<int>>dp(s1.length()+1, vector<int>(s2.length()+1));
+        for(int i=1; i<=s1.size(); i++){
+            dp[i][0] += s1[i-1] + dp[i-1][0];
+        }
+        for(int i=1; i<=s2.size(); i++){
+            dp[0][i] += s2[i-1] + dp[0][i-1];
+        }
+       for(int i1=1; i1<=s1.length(); i1++){
+          for(int i2=1; i2<=s2.length(); i2++){
+             
+             if(s1[i1-1]==s2[i2-1]){
+                 dp[i1][i2] = dp[i1-1][i2-1];
+             }
+              else{
+                 dp[i1][i2] = min({s1[i1-1]+dp[i1-1][i2], s2[i2-1]+dp[i1][i2-1]});        
+              }
+          }
+      }
+        // for(auto &d:dp){
+        //     for(auto &v:d){
+        //         cout<<v<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp.back().back();
     }
 };
