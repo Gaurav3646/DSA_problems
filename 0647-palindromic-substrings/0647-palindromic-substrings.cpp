d0f3e0d005a0dfa56beb5a94e421d71c165e42b0
Gaurav Verma <91468455+Gaurav3646@users.noreply.github.com>
2023-11-02 12:18:47 +0530

    Time: 28 ms (25.50%), Space: 20.5 MB (18.21%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    // int memo[1000][1000] = {0};
-    // int longestPalindromeSubseq(const string& s) {
-    //     return dp(s, 0, s.size() - 1);
-    // }
-
     int countSubstrings(string s) {
-        int n = s.length();
-      vector dp(n, vector<int>(n , 0));
-        int count =0;
-        for(int i=0; i<n; i++){
-            for(int j=i; j>=0; j--){
-                if(j==i){
-                    dp[j][i] = 1;
-                }
-                else if(j+1==i){
-                    dp[j][i] = s[j]==s[i];
-                }
-                else{
-                    dp[j][i] = s[j]==s[i]&&dp[j+1][i-1];
-                }
-                // cout<<dp[j][i]<<j<<" "<<i<<endl;
-              count += dp[j][i];  
-            }
+        
+        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, 0));
+          int ans=0;
+        for(int i=0; i<s.size(); i++){
+            dp[i][i] = 1;
+            ans++;
         }
-        return count;
+       for(int i=0; i<s.size()-1; i++){
+            dp[i][i+1] = s[i]==s[i+1];
+            ans += s[i]==s[i+1];
+       }
+        
+        for(int gap=2; gap<s.size(); gap++){
+          for(int i=0; i<s.size()-gap; i++){
+              int j= i+gap;
+              dp[i][j] = s[i]==s[j]?dp[i+1][j-1]:0;
+             ans += dp[i][j]; 
+          }  
+        }
+        return ans;
+       
     }
 };
